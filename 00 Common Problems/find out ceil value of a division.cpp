37e3dbc2f5d6240b43c70becc964d46cedd1791b
sabbirhasaann <sabbirhasan9989@gmail.com>
2025-08-17 15:31:56 +0600

    ceil value of a division

---
+++ 00 Common Problems/find out ceil value of a division.cpp
@@ -0,0 +1,23 @@
+// 2025/08/17 15:29:32
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+int ceil_division(int a, int b) {
+    return (a + b - 1) / b;
+}
+void soln()
+{
+    cout << ceil_division(10,3);
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    soln();
+    return 0;
+}
