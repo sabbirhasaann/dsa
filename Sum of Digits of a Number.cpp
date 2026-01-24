9673dec332a6a8cb738650660b25c9053d7d25dc
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-01-24 09:19:17 +0600

    added another solution

--- Sum of Digits of a Number.cpp
+++ Sum of Digits of a Number.cpp
@@ -11,9 +11,30 @@ int sumOfDigits(int n){
     return sum;
 }
 
+int sumOfDigitsRec(int n){
+    if(n == 0)
+        return 0;
+    return n % 10 + sumOfDigits(n/10);
+}
+
+
+int sumOfDigitsFromString(int n) {
+    
+    string s = to_string(n);  
+    int sum = 0;
+
+    for (char ch : s) {
+        sum += ch - '0';
+    }
+    return sum;
+}
+
+
 int main()
 {
     int n;
     cin >> n;
-    cout << sumOfDigits(n);
+    cout << sumOfDigits(n)<<endl;
+    cout << sumOfDigitsRec(n) << endl;
+    cout << sumOfDigitsFromString(n) << endl;
 }
