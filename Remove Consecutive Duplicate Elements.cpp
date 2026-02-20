d54faa7b29f0fa0bcef92a3ac3e820da2ca22d4a
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-20 15:45:31 +0600

    added inplace technique

--- Remove Consecutive Duplicate Elements.cpp
+++ Remove Consecutive Duplicate Elements.cpp
@@ -1,8 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// see this for string
+// https://www.geeksforgeeks.org/dsa/remove-consecutive-duplicates-string/
+
+int removeConsecutiveDuplicateInPlace(vector<int> &a){
+    int n = a.size(),k=1;
+    
+    for(int i=1;i<n;++i){
+        if(a[i-1] != a[i]){
+            a[k] = a[i];
+            ++k;
+        }
+    }
+    
+    return k;
+}
+
+
 vector<int> removeConsecutiveDuplicate(vector<int> &a){
-    int k = 0;
     int n = a.size(),i;
     vector<int> res;
     for(i=1;i<n;++i){
@@ -23,6 +39,10 @@ int main()
 {
     vector<int> a = {1,3,3,1,5,5,5,1,1,3};
     a = {1,1,2,2,2,3,1};
-    vector<int> aprime = removeConsecutiveDuplicate(a);
-    printArray(aprime);
+    // vector<int> aprime = removeConsecutiveDuplicate(a);
+    // printArray(aprime);
+
+    int l = removeConsecutiveDuplicateInPlace(a);
+    for(int i=0;i<l;++i)
+        cout << a[i] << " ";
 }
