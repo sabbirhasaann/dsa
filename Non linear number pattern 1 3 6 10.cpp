0aea3f3ffef940aad5d4c18c1c8a38c8842005e2
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-07 02:54:54 +0600

    solved

---
+++ Non linear number pattern 1 3 6 10.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    int n=4, x = 1;
+    // cin >> n;
+    for(int i=1;i<=4;++i){
+        cout << x <<" ";
+        x = x + i + 1;
+    }
+}
